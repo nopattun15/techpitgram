51d9ec4573bb535b11e238a016988edb757e8dc0
takumi.ito <nopattun15@gmail.com>
2021-03-18 10:05:34 +0900

    Posts function

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.5'
 
 gem 'devise'
+gem 'carrierwave', '~> 2.0'
+gem 'mini_magick'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.3'
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'pages#home'
+  root 'posts#index'
   get 'users/:id', to: 'users#show', as: 'user'
+
+  resources :posts, only: [:index, :new, :create] do
+    resources :photos, only: [:create]
+  end
 end
